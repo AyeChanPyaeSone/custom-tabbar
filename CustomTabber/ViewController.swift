4d3a00af457e4a1167d9369a88c87d7708a2c818
Kyaw Myint Thein <kyawmyintthein2020@gmail.com>
2015-07-12 00:36:39 +0800

    Initial Commit

---
+++ CustomTabber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTabber
+//
+//  Created by Kyaw Myint Thein on 7/12/15.
+//  Copyright (c) 2015 com.acps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
